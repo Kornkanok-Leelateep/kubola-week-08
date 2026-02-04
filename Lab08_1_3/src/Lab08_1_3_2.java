66590676a3df67bc2fe48146a08069df480cba60
Kornkanok-Leelateep <68160313@go.buu.ac.th>
2026-02-04 14:45:30 +0700

    Add files via upload

---
+++ Lab08_1_3/src/Lab08_1_3_2.java
@@ -0,0 +1,17 @@
+
+public class Lab08_1_3_2 {
+    public static void main(String[] argsr) {
+        Student1 s1;
+
+        s1 = new Student1();
+
+        s1.readData();
+
+        s1.findTotal();
+
+        s1.computeGrade();
+
+        s1.showReport();
+
+    }
+}
---
+++ Lab08_1_3/src/Student1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Student1 {
+    int id;
+    String firstname,lastname;
+    int mid,fin,hw,total;
+    char grade;
+
+    public void readData() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("This Program to compute grade.");
+        System.out.println("Enter Student ID : ");
+        id = s.nextInt();
+        System.out.println("Enter Firstname : ");
+        firstname = s.next();
+        System.out.println("Enter Lastname : ");
+        lastname = s.next();
+        System.out.println("Enter Midterm[30] : ");
+        mid = s.nextInt();
+        System.out.println("Enter Final[30] : ");
+        fin = s.nextInt();
+        System.out.println("Enter Homework[40] : ");
+        hw = s.nextInt();
+    }
+    public void findTotal() {
+        total = mid+fin+hw;
+    }
+    public void computeGrade() {
+        if (total >= 80)
+            grade = 'A';
+        else if (total >= 70)
+            grade = 'B';
+        else if (total >= 60)
+            grade = 'C';
+        else if (total >= 50)
+            grade = 'D';
+        else grade = 'F';
+    }
+    public void showReport() {
+        System.out.println("------------------- Grade Report -------------------");
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + firstname + " " + lastname);
+        System.out.println("Midterm : " + mid);
+        System.out.println("Final : " + fin);
+        System.out.println("Homework : " + hw);
+        System.out.println("----------------------------------------------------");
+        System.out.println("Total : " + total);
+        System.out.println("Grade : " + grade);
+        System.out.println("----------------------------------------------------");
+    }
+}
